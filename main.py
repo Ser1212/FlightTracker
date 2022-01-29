deba977a121b8daca52b2cafdd130ee940a5ade5
Ser1212 <sergionajm@gmail.com>
2022-01-29 11:07:05 -0500

    hello

---
+++ main.py
@@ -0,0 +1,9 @@
+def main_wrapper():
+    print(f"This is the start of our python project. This function's name is {main_wrapper.__name__}")
+
+    print("This is the end of out python project.")
+
+
+
+if __name__ == "__main__":
+    main_wrapper()
